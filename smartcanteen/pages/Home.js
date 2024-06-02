cd2191dc1ad9d96545e44798792d79fdb136b592
Harish <harishkumarsp023@gmail.com>
2024-06-02 14:08:59 +0530

    feat: Add SVG assets and update dependencies
    This commit adds two new SVG assets, `bg2.svg` and `wavebg.svg`, to the `smartcanteen/assets` directory. It also updates the `react-native-paper` dependency to version `5.12.3` and adds the `react-native-svg` dependency with version `15.2.0` to the `package.json` file. These changes are necessary for improving the visual appearance of the app and enabling SVG support.

--- smartcanteen/pages/Home.js
+++ smartcanteen/pages/Home.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import {  Text, View } from 'react-native';
+import {  Text, View,ImageBackground,ScrollView } from 'react-native';
 import { Carosual } from '../components/Orders/Carosual';
+import { Navbar } from '../ui/Home/Navbar';
+import Category from '../ui/Home/Catagory';
+import { Info } from '../ui/Home/Info';
+import { Asset } from 'expo-asset';
 export const Home = () => {
+ 
   return (
-    <View className="">
-    
-<Carosual/>
-</View>
+  //   <ImageBackground  source={Asset.fromModule(require('../assets/bg2.png'))} style={{flex: 1}}>
+  //   <Navbar/>
+  //   {/* <Carosual/> */}
+  // </ImageBackground>
+  <View className="bg-rose-100/50 h-full">
+    <Navbar/>
+    {/* <Catagory/> */}
+    <View className="flex flex-row justify-between mx-4 p-4">
+      <Text className="text-black font-bold text-lg">Catagory</Text>
+      <Text  className="text-gray-600 font-bold text-sm">See All</Text>
+    </View>
+   
+
+     
+     
+          <Category  />
+      <Info/>
+   
+  </View>
   )
 }
---
+++ smartcanteen/ui/Home/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {  Text, View } from 'react-native';
+import { Image } from 'expo-image';
+import { Asset } from 'expo-asset';
+export const Navbar = () => {
+  return (
+    <View style={{ marginTop: 38, padding: 1 }} className="bg-rose-600  p-4 flex flex-row justify-between rounded-lg mx-6 drop-shadow-xl">
+        <View>
+        <Text className="text-white font-bold text-lg rop-shadow-xl">Hii,Harish</Text>
+      <Text className="text-white rop-shadow-xl">smart canteen</Text>
+        </View>
+     
+      
+     <Image
+        className=" w-14 h-14 rounded-full rop-shadow-xl"
+        source={Asset.fromModule(require('../../assets/crousel1.jpg'))}
+        // style={{ width: "100%", height: 200 }}
+        // contentFit="cover"
+        transition={1000}
+      />
+
+</View>
+  )
+}
